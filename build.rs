25de98e96fdc7a2ba8c9686020f48b9ae53391bb
Haruaki Tamada <tamada@users.noreply.github.com>
2024-05-04 08:14:01 +0900

    update for using app name from Cargo.toml in build.rs (remove peculiar name of application from build.rs)

--- build.rs
+++ build.rs
@@ -5,24 +5,36 @@ use std::path::Path;
 
 include!("src/cli.rs");
 
-fn generate(s: Shell, app: &mut Command, outdir: &Path, file: &str) {
+fn generate(s: Shell, app: &mut Command, appname: &str, outdir: &Path, file: String) {
     let destfile = outdir.join(file);
-    println!("dest: {}", destfile.display());
     std::fs::create_dir_all(destfile.parent().unwrap()).unwrap();
     let mut dest = File::create(destfile).unwrap();
+    
+    clap_complete::generate(s, app, appname, &mut dest);
+}
 
-    clap_complete::generate(s, app, "totebag", &mut dest);
+fn parse_cargo_toml() -> toml::Value {
+    let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("Cargo.toml");
+    let file = match std::fs::read_to_string(path) {
+        Ok(f) => f,
+        Err(e) => panic!("{}", e),
+    };
+
+    file.parse().unwrap()
 }
 
 fn main() {
+    let table = parse_cargo_toml();
+    let appname = table["package"]["name"].as_str().unwrap();
+
     let mut app = CliOpts::command();
-    app.set_bin_name("totebag");
+    app.set_bin_name(appname);
 
     let outdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("target/completions/");
 
-    generate(Shell::Bash, &mut app, &outdir, "bash/totebag");
-    generate(Shell::Elvish, &mut app, &outdir, "elvish/totebag");
-    generate(Shell::Fish, &mut app, &outdir, "fish/totebag");
-    generate(Shell::PowerShell, &mut app, &outdir, "powershell/totebag");
-    generate(Shell::Zsh, &mut app, &outdir, "zsh/_totebag");
+    generate(Shell::Bash, &mut app, appname, &outdir, format!("bash/{}", appname));
+    generate(Shell::Elvish, &mut app, appname, &outdir, format!("elvish/{}", appname));
+    generate(Shell::Fish, &mut app, appname, &outdir, format!("fish/{}", appname));
+    generate(Shell::PowerShell, &mut app, appname, &outdir, format!("powershell/{}", appname));
+    generate(Shell::Zsh, &mut app, appname, &outdir, format!("zsh/_{}", appname));
 }
